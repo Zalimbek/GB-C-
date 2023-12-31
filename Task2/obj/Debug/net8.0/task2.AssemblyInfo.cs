91c54b87ea3c3dbab8a8ef6e8ae8f7003fd04b51
Zhamilia Alimbekova <jamilysoul@mail.ru>
2023-12-31 19:19:12 +0300

    quotion

--- Task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6114963da5515a88e19fa829895f6683f9ab90e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb64ad9877798dec97752a57cb1a95f71a9066b")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
